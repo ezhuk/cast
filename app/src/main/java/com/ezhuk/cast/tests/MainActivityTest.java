37eb05288d1b48c9ff2c095aaf2f7ae997e78e6f
Eugene Zhuk <eugene.zhuk@gmail.com>
2014-02-28 22:50:58 -0800

    More tests.

--- app/src/main/java/com/ezhuk/cast/tests/MainActivityTest.java
+++ app/src/main/java/com/ezhuk/cast/tests/MainActivityTest.java
@@ -6,15 +6,21 @@ package com.ezhuk.cast.tests;
 
 import android.app.Fragment;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.ezhuk.cast.DevicesFragment;
 import com.ezhuk.cast.MainActivity;
 import com.ezhuk.cast.R;
 
+import java.lang.reflect.Array;
+
 
 public class MainActivityTest
         extends ActivityInstrumentationTestCase2<MainActivity> {
     private MainActivity mMainActivity;
+    private String[] mDrawerArray;
+    private ListView mDrawerList;
 
     public MainActivityTest() {
         super(MainActivity.class);
@@ -24,11 +30,27 @@ public class MainActivityTest
     protected void setUp() throws Exception {
         super.setUp();
         mMainActivity = getActivity();
+        mDrawerArray = mMainActivity.getResources().getStringArray(R.array.drawer_array);
+        mDrawerList = (ListView) mMainActivity.findViewById(R.id.drawer_list);
+    }
+
+    public void testPreconditions() {
+        assertNotNull("mMainActivity is Null", mMainActivity);
+        assertNotNull("mDrawerArray is Null", mDrawerArray);
+        assertNotNull("mDrawerList is Null", mDrawerList);
     }
 
     public void testMainActivityTestDrawer_defaultFragment() {
         Fragment fragment = mMainActivity.getFragmentManager()
                 .findFragmentById(R.id.drawer_frame);
         assertEquals(DevicesFragment.class, fragment.getClass());
+    }
+
+    public void testMainActivityTestDrawer_defaultList() {
+        assertEquals(mDrawerArray.length, mDrawerList.getChildCount());
+        for (int i = 0; i < mDrawerArray.length; ++i) {
+            assertEquals(mDrawerArray[i], ((TextView) mDrawerList
+                    .getChildAt(i)).getText().toString());
+        }
     }
 }
